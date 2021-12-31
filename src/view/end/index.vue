a6686913615556ae4edd22291edb9255dba9415f
warlordlee <286527307@qq.com>
2021-12-31 10:37:12 +0800

    新年快乐

--- src/view/end/index.vue
+++ src/view/end/index.vue
@@ -20,8 +20,26 @@
         <div class="title-text">
         </div>
         <div class="dsp">
-          <img src="../../assets/end/tiger-pic.png" class="t-pic">
+            <img src="../../assets/end/tiger-pic.png" class="t-pic">
+            <template v-for="(text,index) in texts">
+                <div :key="'text'+index" class="text" :class="0===index?'mg-top':''">
+                    {{text}}
+                </div>
+            </template>
         </div>
+        <div class="tools">
+            <img src="../../assets/end/heart.png">
+            <img src="../../assets/end/say.png">
+        </div>
+        <div class="wish">
+            2022升职加薪工作如虎添翼
+        </div>
+        <div class="wish">
+            新的一年头发依旧茂密！
+        </div>
+        <img src="../../assets/end/bottom.png" class="bottom">
+        <img src="../../assets/end/tiger2.png" class="tiger2">
+        <img src="../../assets/end/money.png" class="money">
     </div>
 </template>
 
@@ -30,7 +48,19 @@
         name: "index",
         data() {
             return {
-                title: ["虎", "力", "全", "开"]
+                title: ["虎", "力", "全", "开"],
+                texts:[
+                    "过去一年你写下了43252行代码",
+                    "热爱使用javascript的你",
+                    "一定是个向往自由的人",
+                    "你经常debug到深夜",
+                    "甚至直到天亮",
+                    "新的一年了",
+                    "保重身体",
+                    "毕竟",
+                    "世界辣么大",
+                    "总得去看看"
+                ]
             }
         }
     }
@@ -86,7 +116,8 @@
             margin-top: 3px;
         }
     }
-    .title{
+
+    .title {
         position: absolute;
         top: 110px;
         display: flex;
@@ -97,7 +128,8 @@
         margin: 0 auto;
         padding-left: 25px;
     }
-    .circle{
+
+    .circle {
         background-color: #F7D000;
         border-radius: 50%;
         width: 75px;
@@ -108,7 +140,8 @@
         position: relative;
 
     }
-    .title-text{
+
+    .title-text {
         background-color: #F7D000;
         position: absolute;
         width: 207px;
@@ -119,7 +152,8 @@
         z-index: 200;
         height: 35px;
     }
-    .title-text-font{
+
+    .title-text-font {
         position: absolute;
         width: 100%;
         height: 100%;
@@ -127,19 +161,73 @@
         font-size: 42px;
         line-height: 75px;
     }
-    .dsp{
+
+    .dsp {
         width: 90%;
-        height: 500px;
+        height: 450px;
         top: 172px;
         margin-left: 5%;
         background: url("../../assets/end/bg.jpg");
         background-size: 100% 100%;
         margin-top: 100px;
     }
-    .t-pic{
+
+    .t-pic {
         position: absolute;
         width: 200px;
-        top: 440px;
-        right: 100px;
+        top: 400px;
+        right: 60px;
+    }
+
+    .bottom {
+        position: absolute;
+        bottom: -20px;
+        left: 0;
+        width: 100%;
+    }
+
+    .tools {
+        display: flex;
+        padding: 10px 15px;
+        img{
+            width: 30px;
+            height: 30px;
+            margin-right: 10px;
+        }
+    }
+    .wish{
+        border-radius: 8px;
+        line-height: 30px;
+        background-color: #4DB560;
+        border: 2px solid black;
+        text-align: left;
+        width: 65%;
+        margin-left: 19px;
+        padding-left: 15px;
+        margin-bottom: 7px;
+        font-size: 18px;
+    }
+    .text{
+        color: #f3b609;
+        text-align: left;
+        padding: 2px 15px;
+        font-size: 20px;
+        font-weight: bolder;
+        letter-spacing: 2px;
+    }
+    .mg-top{
+        padding-top: 30px;
+    }
+    .tiger2{
+        position: absolute;
+        right: 16px;
+        bottom: 61px;
+        width: 72px;
+    }
+    .money{
+        position: absolute;
+        right: 36px;
+        bottom: 64px;
+        width: 26px;
     }
 </style>
